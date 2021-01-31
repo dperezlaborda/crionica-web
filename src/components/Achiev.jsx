17e56f2d260247b720ed1531e25ee50f63cbc057
dperezlaborda <55368538+dperezlaborda@users.noreply.github.com>
2021-01-31 01:11:10 -0300

    se agregan retoques de estilo a objetivos

---
+++ src/components/Achiev.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Achiev = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Achiev
---
+++ src/sections/AchievContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AchievContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AchievContainer
